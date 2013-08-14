2993745225223992bbb10b0e28a96ce3f976222f
Kirt Bush <kirtbush1@gmail.com>
2013-08-13 22:12:20 -0400

    initial version

--- main.cpp
+++ main.cpp
@@ -28,32 +28,68 @@ SDL_Texture * LoadImage( std::string file )
     return texture;
 }
 
+void ApplySurface( int x, int y, SDL_Texture *tex, SDL_Renderer *rend )
+{
+    SDL_Rect pos;
+    pos.x = x;
+    pos.y = y;
+    SDL_QueryTexture( tex, NULL, NULL, &pos.w, &pos.h );
+    SDL_RenderCopy( rend, tex, NULL, &pos );
+}
+
 int main( int argc, char* args[] )
 {
         //Start SDL
-    SDL_Init( SDL_INIT_EVERYTHING );
-
-
-    window = SDL_CreateWindow( "HellowWorld!", 100, 100, 640, 480, SDL_WINDOW_SHOWN );
-
-
-    renderer = SDL_CreateRenderer( window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC );
-
-    SDL_Texture *tex = NULL;
-    tex = LoadImage( "res/helloworldies.bmp" );
-    if( tex == NULL )
+    if ( SDL_Init( SDL_INIT_EVERYTHING ) == -1)
     {
         return 1;
     }
 
+    window = SDL_CreateWindow( "HelloWorld!", SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+
+    renderer = SDL_CreateRenderer( window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC );
+
+    SDL_Texture *background = NULL, *face = NULL;
+    background = LoadImage( "res/background.bmp" );
+    if( background == NULL )
+    {
+        return 2;
+    }
+
+    face = LoadImage( "res/face.bmp" );
+    if( face == NULL )
+    {
+        return 3;
+    }
+
     SDL_RenderClear( renderer );
-    SDL_RenderCopy( renderer, tex, NULL, NULL );
+
+    int bW, bH;
+    SDL_QueryTexture(background, NULL, NULL, &bW, &bH);
+    for(int idx=0; idx < 2; idx++ )
+        for(int idy=0; idy < 2; idy++ )
+            ApplySurface( idx * bW, idy * bH, background, renderer );
+
+//    ApplySurface( 0, 0, background, renderer );
+//    ApplySurface( bW, 0, background, renderer );
+//    ApplySurface( 0, bH, background, renderer );
+//    ApplySurface( bW, bH, background, renderer );
+
+    int fW, fH;
+    SDL_QueryTexture(face, NULL, NULL, &fW, &fH);
+    int x = SCREEN_WIDTH / 2 - fW / 2;
+    int y = SCREEN_HEIGHT / 2 - fH / 2;
+    ApplySurface(x, y, face, renderer);
+
+    //SDL_RenderCopy( renderer, tex, NULL, NULL );
     SDL_RenderPresent( renderer );
 
     SDL_Delay( 2000 );
 
     //cleanup yo s
-    SDL_DestroyTexture( tex );
+    SDL_DestroyTexture( background );
+    SDL_DestroyTexture( face );
     SDL_DestroyRenderer( renderer );
     SDL_DestroyWindow( window );
 
